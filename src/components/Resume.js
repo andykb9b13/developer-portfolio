c0d6f026ea2579916b774ff83ba460f6c6a7fda3
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-04-25 23:12:44 -0400

    added resume download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Icons from "./Icons";
+import ResumePdf from "../assets/Resume.pdf";
+import ResumeImg from "../assets/Resume.png";
+import { Button } from "react-bootstrap";
+
+const styles = {
+  icon: {
+    boxShadow: "5px 5px 10px #222",
+    marginBottom: "20px",
+    borderRadius: "5px",
+    width: "30%",
+  },
+};
 
 const Resume = () => {
   return (
-    <div className="section">
+    <div className="section justify-center">
       <h2>Technologies</h2>
+      <Button>
+        <a href={ResumePdf} download="Resume">
+          <img src={ResumeImg} alt="resume icon" style={styles.icon} />
+        </a>
+      </Button>
       <div>
         <Icons />
       </div>
