34fdcfa782d55ca7ada3c65ac7cd02bb6ed39de2
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-21 11:24:39 -0400

    changed icon and projectCard animations to be staggered instead of random

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -63,19 +63,33 @@ const ProjectCard = () => {
     },
   ];
 
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+
   return (
-    <div className="flex flex-wrap justify-center">
+    <motion.div
+      className="flex flex-wrap justify-center"
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
       {apps.map((app, i) => (
         <motion.div
           className="projectContainer m-2 relative"
           key={i}
-          initial={{ opacity: 0, scale: 0.1 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 1,
-            delay: Math.random() * 2,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
+          variants={item}
         >
           <div className="imgContainer">
             <img
@@ -97,7 +111,7 @@ const ProjectCard = () => {
           </div>
         </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
